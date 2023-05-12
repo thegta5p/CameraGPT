4d9df779e318734c4e7342b0e96fb0d71ee17301
Isaac Castro <icast065@ucr.edu>
2023-05-12 11:49:09 -0700

    Initial Commit

---
+++ CameraGPT/CameraGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraGPTApp.swift
+//  CameraGPT
+//
+//  Created by Isaac Castro on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
